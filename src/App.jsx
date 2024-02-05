12a98a0167d03a36c49c5dce4ff3cf18d58a8608
Joseph Russo <joe@emergency.company>
2024-02-05 13:11:44 -0500

    James Can't Spell

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="text-center flex flex-col items-center">
       <img src={viteLogo} className="h-16 w-16 mb-6 mt-8" alt="Vite logo" />
-      <h1 className="text-white text-xl mb-2 font-bold">Emegency Company</h1>
+      <h1 className="text-white text-xl mb-2 font-bold">Emergency Company</h1>
       <p className="read-the-docs">Coming soon...</p>
       <a className="read-the-docs" href="mailto:team@emergency.company">team@emergency.company</p>
     </div>
